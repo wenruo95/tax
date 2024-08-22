71b1cbe1deefac6eebb5317368e80a88c9f56c5a
zemanzeng(曾巍) <zemanzeng@futunn.com>
2024-08-22 16:41:44 +0800

    月薪计算

--- const.go
+++ const.go
@@ -22,14 +22,17 @@ func P100(x float64) float64 {
 var (
 	SocialAverateSalary float64 = 14553 // 深圳3倍社平
 
-	ElderlyCareInsuranceRate  = []float64{P100(13 + 1), P100(8)}  // 养老保险
+	ElderlyCareInsuranceRate = []float64{P100(15), P100(8)} // 养老保险
+	//ElderlyCareInsuranceRate  = []float64{P100(13 + 1), P100(5)}  // 养老保险
 	MediacalInsuranceRate     = []float64{P100(6 + 0.2), P100(2)} // 医疗保险
 	MaternityInsuranceRate    = []float64{P100(0.5), P100(0)}     // 生育保险
 	UnemploymentInsuranceRate = []float64{P100(1), P100(0.5)}     // 失业保险
 	WorkInjuryInsuranceRate   = []float64{P100(0.14), P100(0)}    // 工伤保险
 	HousingProvidentFundRate  = []float64{P100(12), P100(12)}     // 住房公积金
+	//HousingProvidentFundRate = []float64{P100(10), P100(10)} // 住房公积金
 
 	HousingProvidentFundReturnRate = 100 / 100 // 住房公积金返回比例
 
-	TaxFreeSalary float64 = 60000 // 专项附加扣除
+	//TaxFreeSalary float64 = 60000 // 专项附加扣除
+	TaxFreeSalary float64 = 0 // 专项附加扣除
 )
--- main.go
+++ main.go
@@ -12,8 +12,6 @@ func CalculateTax(salary float64, rates []*TaxRateData) float64 {
 		rate := rates[i]
 		tax := math.Min(salary-rate.L, rate.R-rate.L) * rate.Rate
 		sum = sum + tax
-		//fmt.Printf("[L:%v R:%v Rate:%0.2f] tax:%v sum:%v current_rate:%0.2f%%\n",
-		//	rate.L, rate.R, rate.Rate, tax, sum, sum*100/salary)
 	}
 	return sum
 }
@@ -30,7 +28,7 @@ func main() {
 	}
 
 	base := math.Min(msalary, 3*SocialAverateSalary)
-	mtax1 := []float64{
+	tax1List := []float64{
 		base * ElderlyCareInsuranceRate[1],
 		base * MediacalInsuranceRate[1],
 		base * MaternityInsuranceRate[1],
@@ -38,22 +36,50 @@ func main() {
 		base * WorkInjuryInsuranceRate[1],
 		base * HousingProvidentFundRate[1],
 	}
-	var mtax1sum float64
-	for _, v := range mtax1 {
-		mtax1sum += v
+	tax1Listsum := tax1List[0] + tax1List[1] + tax1List[2] + tax1List[3] + tax1List[4] + tax1List[5]
+
+	{
+		var tax2sum float64
+		for i := 0; i < 12; i++ {
+			tax1 := tax1Listsum
+			tax2 := CalculateTax((msalary-tax1Listsum)*float64(i+1)-tax2sum, TaxRates) - tax2sum
+			tax2Return := base * (HousingProvidentFundRate[0] + HousingProvidentFundRate[1])
+			left := msalary - tax1 - tax2
+			fmt.Printf("[%2d] 月薪:%0.2f 五险一金:%0.2f 扣税:%0.2f 到手:%0.2f 税率:%0.2f%% 额外公积金:%0.2f\n",
+				i+1, msalary, tax1, tax2, left, (msalary-left)*100/msalary, tax2Return)
+			tax2sum = tax2sum + tax2
+		}
 	}
 
-	salary := msalary*(12+bonus) + stock
-	tax1 := mtax1sum * 12
-	tax2 := CalculateTax(salary-tax1-TaxFreeSalary, TaxRates)
-	tax2Return := base * (HousingProvidentFundRate[0] + HousingProvidentFundRate[1]) * 12
-	left := salary - tax1 - tax2 + tax2Return
+	{
+		// year
+		salary := msalary*(12+bonus) + stock
+		tax1 := tax1Listsum * 12
+		tax2 := CalculateTax(salary-tax1-TaxFreeSalary, TaxRates)
+		tax2Return := base * (HousingProvidentFundRate[0] + HousingProvidentFundRate[1]) * 12
+		left := salary - tax1 - tax2 + tax2Return
+		fmt.Printf("月薪(%0.2f) + 奖金(%0.2f) + 股票(%0.2f) = 年包(%0.2f)\n",
+			msalary, bonus*msalary, stock, salary)
+		fmt.Printf("养老保险(%0.2f) + 医疗保险(%0.2f) + 生育保险(%0.2f) + 失业保险(%0.2f) + 工伤保险(%0.2f)"+
+			" + 住房公积金(%0.2f) = 五险一金(%0.2f), 一年共计:%0.2f\n",
+			tax1List[0], tax1List[1], tax1List[2], tax1List[3], tax1List[4], tax1List[5], tax1Listsum, tax1)
+		fmt.Printf("年薪:%0.2f 五险一金:%0.2f 扣税:%0.2f 公积金返还:%0.2f 到手:%0.2f 税率:%0.2f%%\n",
+			salary, tax1, tax2, tax2Return, left, (salary-left)*100/salary)
+	}
 
-	fmt.Printf("月薪(%0.2f) + 奖金(%0.2f) + 股票(%0.2f) = 年包(%0.2f)\n",
-		msalary, bonus*msalary, stock, salary)
-	fmt.Printf("养老保险(%0.2f) + 医疗保险(%0.2f) + 生育保险(%0.2f) + 失业保险(%0.2f) + 工伤保险(%0.2f)"+
-		" 住房公积金(%0.2f) = 五险一金(%0.2f), 一年共计:%0.2f\n",
-		mtax1[0], mtax1[1], mtax1[2], mtax1[3], mtax1[4], mtax1[5], mtax1sum, tax1)
-	fmt.Printf("年薪:%0.2f 五险一金:%0.2f 扣税:%0.2f 公积金返还:%0.2f 到手:%0.2f 税率:%0.2f%%\n",
-		salary, tax1, tax2, tax2Return, left, (1-left/salary)*100)
+	{
+		// company
+		ctaxList := []float64{
+			base * ElderlyCareInsuranceRate[0],
+			base * MediacalInsuranceRate[0],
+			base * MaternityInsuranceRate[0],
+			base * UnemploymentInsuranceRate[0],
+			base * WorkInjuryInsuranceRate[0],
+			base * HousingProvidentFundRate[0],
+		}
+		ctaxListsum := ctaxList[0] + ctaxList[1] + ctaxList[2] + ctaxList[3] + ctaxList[4] + ctaxList[5]
+		fmt.Printf("[公司] 养老保险(%0.2f) + 医疗保险(%0.2f) + 生育保险(%0.2f) + 失业保险(%0.2f) + 工伤保险(%0.2f)"+
+			" + 住房公积金(%0.2f) = 五险一金(%0.2f), 一年共计:%0.2f\n",
+			ctaxList[0], ctaxList[1], ctaxList[2], ctaxList[3], ctaxList[4], ctaxList[5], ctaxListsum, ctaxListsum*12)
+	}
 }
